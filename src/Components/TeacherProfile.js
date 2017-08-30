4bf934b5363006934d074450a9fce96682e1d458
Hanan Kavitz <hananke0@gmail.com>
2017-08-30 17:15:20 +0300

    TeacherProfile added

---
+++ src/Components/TeacherProfile.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class TeacherProfile extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Teacher profile</h2>
+            </div>
+        );
+    }
+}
+
+export default TeacherProfile;
