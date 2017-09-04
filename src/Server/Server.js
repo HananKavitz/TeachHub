da4877e4ddaee2df5bd27d2670cf4b3196d58183
Hanan Kavitz <Hanan_Kavitz@amat.com>
2017-09-04 18:23:02 +0300

    added features to Server.js

--- src/Server/Server.js
+++ src/Server/Server.js
@@ -4,7 +4,9 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var fs = require('fs');
 var router = require('./Routes/Routes');
-
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 //var mongoose = require('mongoose');
 
 var hostname = 'localhost';
@@ -15,7 +17,12 @@ var app = express();
 //mongoose.connect('mongodb://localhost/mongoData');
 
 app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(path.join(__dirname, '../../build')));
 app.use('/', router);
