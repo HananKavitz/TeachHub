7fdedbb075a602c2a61e818d4a52a48ba753078f
Hanan Kavitz <hananke0@gmail.com>
2017-09-10 23:13:03 +0300

    scroll up button added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -17,6 +17,28 @@ export default class Home extends Component {
                     <h1>
                         welcome to teachub
                     </h1>
+                </div>
+            </div>
+            <br/>
+            <div className="row row-content">
+                <div className="bluebox col-sm-6 col-xs-12">
+                    <h3>Best of all times</h3>
+                    <UnderConstruction />
+                </div>
+
+                <div className="bluebox col-sm-6 col-xs-12">
+                    <h3>New teaching aids</h3>
+                    <UnderConstruction />
+                </div>
+
+                <br/>
+                <div className="bluebox col-sm-6 col-xs-12">
+                    <h3>Best teachers</h3>
+                    <UnderConstruction />
+                </div>
+
+                <div className="bluebox col-sm-6 col-xs-12">
+                    <h3>Peek of the week</h3>
                     <UnderConstruction />
                 </div>
             </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import MyCarousel from './MyCarousel';
 import MyNav from './MyNav';
 import Footer from './Footer'
-
+import ScrollToTop from 'react-scroll-up'
 
 export default class Main extends Component {
 
@@ -11,9 +11,15 @@ export default class Main extends Component {
         return (
 
             <div>
+
                 <MyNav/>
                 <br/>
                 <MyCarousel/>
+                <ScrollToTop  showUnder={60}>
+                    <span className = "floatingbotton">
+                        <i className="glyphicon glyphicon-menu-up"/>
+                    </span>
+                </ScrollToTop>
                 <br/>
                 {this.props.children}
                 <br/>
