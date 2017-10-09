8ccd0d1130cd44571bc5d45325c71a4f44dd86b2
Hanan Kavitz <Hanan_Kavitz@amat.com>
2017-10-09 17:08:45 +0300

    get profileData

--- src/Server/Routes/ProfileRoutes.js
+++ src/Server/Routes/ProfileRoutes.js
@@ -27,7 +27,7 @@ router.put('/EditProfileData',Verify.verifyOrdinaryUser,function(req,res,next) {
         userProfile.myCountry = message.myCountry;
         userProfile.gradesITeach = message.gradesITeach;
         console.log(userProfile)
-        userAccount.save(function(err){
+        userProfile.save(function(err){
             console.log(err);
             next(err);
         });
@@ -39,6 +39,20 @@ router.put('/EditProfileData',Verify.verifyOrdinaryUser,function(req,res,next) {
     res.sendStatus(200).
     json({status: 'User profile saved'});
 
+});
+
+router.get('/ProfileData',Verify.verifyOrdinaryUser,function(req,res,next){
+    const token = req.headers['x-access-token'];
+    const decodedUser = jwt.decode(token, {complete: true});
+    UserProfile.findById(decodedUser._id , function(err,userProfile){
+        if (err){
+            res.sendStatus(500).
+            json({status: 'Failed to get user profile'});
+        }
+        //send the user to the client
+        res.sendStatus(200).
+        json({userProfile: JSON.stringify(userProfile)});
+    })
 });
 
 module.exports = router;
