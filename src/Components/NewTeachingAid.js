26b80e345b6da4cd53346133127ee626df90b0ec
Hanan Kavitz <hananke0@gmail.com>
2017-10-11 13:28:25 +0300

    tags added to new teaching aid

--- src/Components/NewTeachingAid.js
+++ src/Components/NewTeachingAid.js
@@ -1,13 +1,73 @@
 import React, {Component} from 'react';
 import UnderConstruction from './UnderConstruction';
 import Form from "react-jsonschema-form";
+import { WithContext as ReactTags } from 'react-tag-input';
 
 export default class NewTeachingAid extends Component {
+    constructor(props){
+        super(props);
+        
+        this.state = {tags : [{}]};
+        
+
+        //mothods bindings
+        this.createNewteachingAid = this.createNewteachingAid.bind(this);
+        this.onErrorSubmit = this.onErrorSubmit.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+        this.handleAddition = this.handleAddition.bind(this);
+        this.handleDrag = this.handleDrag.bind(this);
+    }
+
+    createNewteachingAid(form){
+        
+    }
+
+    onErrorSubmit(err){
+        console.error(err);
+    }
+
+    handleDelete(i) {
+        let tags = this.state.tags;
+        tags.splice(i, 1);
+        this.setState({tags: tags});
+    }
+
+    handleAddition(tag) {
+        let tags = this.state.tags;
+        tags.push({
+            id: tags.length + 1,
+            text: tag
+        });
+        this.setState({tags: tags});
+    }
+
+    handleDrag(tag, currPos, newPos) {
+        let tags = this.state.tags;
+
+        // mutate array
+        tags.splice(currPos, 1);
+        tags.splice(newPos, 0, tag);
+
+        // re-render
+        this.setState({ tags: tags });
+    }
+
     render() {
 
         const newTeachinAidiUiSchema = {
-            "title": {
-                "ui:widget": "password"
+            "tags": {
+                "ui:widget": (props) => {
+                    return (<ReactTags tags = {this.state.tags}
+                                handleDelete={this.handleDelete}
+                                handleAddition={this.handleAddition}
+                                className = {{
+                                            tags : 'ReactTags__tags',
+                                            tagInput: 'tagInputClass',
+                                            selected: 'ReactTags__selected',
+                                            remove: 'ReactTags__remove',                
+                                            }}/>
+                            )
+                }
             }
         };
 
@@ -47,8 +107,8 @@ export default class NewTeachingAid extends Component {
                         <Form
                             schema={newTeachinAidSchema}
                             uiSchema={newTeachinAidiUiSchema}
-                            onSubmit={this.props.loginCallback}
-                            onError={this.props.onErrorSubmit}>
+                            onSubmit={this.createNewteachingAid}
+                            onError={this.onErrorSubmit}>
                         </Form>
                         <UnderConstruction/>
                     </div>
@@ -56,4 +116,6 @@ export default class NewTeachingAid extends Component {
             </div>
         );
     }
+
+   
 }
