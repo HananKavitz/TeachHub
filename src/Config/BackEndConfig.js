cf2e8a5bfe28cdafbe7e9eb87b26020fa38f65be
Hanan Kavitz <Hanan_Kavitz@amat.com>
2017-10-22 09:59:30 +0300

    limits module added

--- src/Config/BackEndConfig.js
+++ src/Config/BackEndConfig.js
@@ -3,5 +3,10 @@ module.exports = {
 	'serverPort' : 3001,
 	'mongodbURL' : 'localhost',
 	'mongodbPort' : 27017,
-	'secretKey' : '12345-67890-09876-54321'
+	'secretKey' : '12345-67890-09876-54321',
+	'limitsConfig' : {
+		enable: true,
+		file_uploads: true,
+		post_max_size: 2000000
+	}
 }
