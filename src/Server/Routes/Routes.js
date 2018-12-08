5d6fa9794fb914158164fa7e58a52947e9c10d35
Hanan Kavitz <hananke0@gmail.com>
2018-12-08 12:53:30 +0200

    dont save passwords in mongo

--- src/Server/Routes/Routes.js
+++ src/Server/Routes/Routes.js
@@ -5,7 +5,8 @@ var userAccount = require('../../database/Models/userAccount');
 var UserProfile = require('../../database/Models/UserProfile');
 var passport = require('passport');
 var Verify = require('./Verify');
-
+var bcrypt = require('bcryptjs');
+var saltRounds = 10;
 
 router.get('/interests',Verify.verifyOrdinaryUser, function(req,res,next) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -32,52 +33,63 @@ router.get('/UserData',Verify.verifyOrdinaryUser, function(req,res,next) {
 });
 
 router.post('/register', function(req,res,next){
-    userAccount.register(
-        new userAccount({
-            username : req.body.username,
-            password : req.body.password,
-            email : req.body.email}),
-            req.body.password,
-            function(err, account) {
-                if (err) {
-                    return res.status(500).
-                    json({ err : err });
-                }
+    
+    const password = req.body.password;
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        if (err){
+            console.log(err);
+            return
+        }
+        userAccount.register(
+            new userAccount({
+                username : req.body.username,
+                password : hash,
+                email : req.body.email}),
+                req.body.password,
+                function(err, account) {
+                    if (err) {
+                        console.log(err);
+                        return res.status(500).
+                        json({ err : err });
+                    }
 
-                passport.authenticate('local')(req, res, function () {
-                    return res.status(200).
-                    json({ status: 'Successful registration!!' });
-                });
-            });
+                    passport.authenticate('local')(req, res, function () {
+                        return res.status(200).
+                        json({ status: 'Successful registration!!' });
+                    });
+                }
+            );
+        });
 
 });
 
 router.post('/Login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.status(401).json({
-        err: info
-      });
-    }
-    req.logIn(user, function(err) {
-      if (err) {
-        return res.status(500).json({
-          err: 'Could not log in user'
+    passport.authenticate('local', function(err, user, info) {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.status(401).json({
+                err: info
         });
-      }
+        }
+        req.logIn(user, function(err) {
+            if (err) {
+                return res.status(500).json({
+                    err: 'Could not log in user'
+                });
+            }
 
-      const token = Verify.getToken(user);
-      //const token = JSON.stringify(user);
-              res.status(200).json({
-        status: 'Login successful!',
-        success: true,
-        token: token
-      });
-    });
-  })(req,res,next);
+            const token = Verify.getToken(user);
+            //const token = JSON.stringify(user);
+            res.status(200).json({
+                status: 'Login successful!',
+                success: true,
+                token: token
+            });
+        });
+    })(req,res,next);
+    
 });
 
 router.post('/Logout', function(req,res,next){
@@ -87,6 +99,7 @@ router.post('/Logout', function(req,res,next){
         status: 'Nice knowing you'
     });
 });
+
 
 
 module.exports = router;
